1e5c3a08c842bcecafdb0b0d05e7d38e7401875c
Emiel Tasseel <emiel.tasseel@gmail.com>
2016-06-17 14:47:34 +0200

    updated? to 2.0.0b1

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,24 @@
 FROM nginx:latest
 
-# Default Configuration
-ENV DB_HOST mysql
-ENV APP_URL polr.me
-ENV APP_NAME Polr
-ENV REG_TYPE none
-ENV ADMIN_USER admin
-ENV ADMIN_PASSWORD secret
-ENV ADMIN_EMAIL admin@example.tld
-ENV SETUP_PASSWORD none
-ENV REG_TYPE none
-ENV IP_METHOD \$_SERVER['REMOTE_ADDR']
-ENV PRIVATE false
-
 RUN apt-get update
 RUN apt-get install -y php5-cli php5-fpm php5-mysqlnd php5-mcrypt git
 RUN rm /etc/nginx/conf.d/*
-ADD dockercfg.php /scripts/
+
+RUN git clone --depth 1  --branch 2.0.0b1 https://github.com/Cydrobolt/polr polr
+
+# Default Configuration
+ENV APP_KEY=DrLeswiahlu3hoUBl6cho2tlU5Pla6t7 APP_NAME=Polr APP_PROTOCOL=http:// APP_ADDRESS=example.com APP_STYLESHEET=
+ENV DB_HOST=localhost DB_PORT=3306 DB_DATABASE=polr DB_USERNAME=root DB_PASSWORD=
+ENV POLR_ALLOW_ACCT_CREATION=false POLR_ACCT_ACTIVATION=false POLR_GENERATED_AT= POLR_SETUP_RAN=true
+ENV ST_PUBLIC_INTERFACE=true ST_SHORTEN_PERMISSION=true ST_INDEX_REDIRECT=/login ST_PASSWORD_RECOV=true ST_AUTO_API=true ST_ANON_API=false ST_BASE=32
+ENV MAIL_HOST= MAIL_PORT= MAIL_USERNAME= MAIL_PASSWORD= MAIL_FROM_ADDRESS= MAIL_FROM_NAME=
+ENV TMP_SETUP_AUTH_KEY=DrLeswiahlu3hoUBl6cho2tlU5Pla6t7
+
+EXPOSE 80
+
+RUN chmod -R 777 /polr
+RUN chown -R www-data /polr
+
 ADD conf/polr.conf /etc/nginx/conf.d/
 ADD run.sh /
 CMD bash /run.sh
--- run.sh
+++ run.sh
@@ -1,14 +1,42 @@
-echo "* Configuring nginx"
-sed -i "s/%SERVER_NAME%/$APP_URL/g" /etc/nginx/conf.d/polr.conf
-echo "* Downloading polr"
-git clone --depth 1  --branch 1.0-legacy https://github.com/Cydrobolt/polr app
-cd app
-cp /scripts/dockercfg.php .
-echo "* Installing polr"
-php dockercfg.php
-rm dockercfg.php
-if [ $SETUP_PASSWORD == "none" ]; then
-    rm setup.php
-fi
+echo "* Configuring polr"
+
+cd polr
+cp .env.setup .env
+cat resources/views/env.blade.php > .env
+
+sed -i "s^{{\\\$APP_KEY}}^$APP_KEY^g" .env
+sed -i "s^{{\\\$APP_NAME}}^$APP_NAME^g" .env
+sed -i "s^{{\\\$APP_PROTOCOL}}^$APP_PROTOCOL^g" .env
+sed -i "s^{{\\\$APP_ADDRESS}}^$APP_ADDRESS^g" .env
+sed -i "s^{{\\\$APP_STYLESHEET}}^$APP_STYLESHEET^g" .env
+
+sed -i "s^{{\\\$DB_HOST}}^$DB_HOST^g" .env
+sed -i "s^{{\\\$DB_PORT}}^$DB_PORT^g" .env
+sed -i "s^{{\\\$DB_DATABASE}}^$DB_DATABASE^g" .env
+sed -i "s^{{\\\$DB_USERNAME}}^$DB_USERNAME^g" .env
+sed -i "s^{{\\\$DB_PASSWORD}}^$DB_PASSWORD^g" .env
+
+sed -i "s^{{\\\$POLR_ALLOW_ACCT_CREATION}}^$POLR_ALLOW_ACCT_CREATION^g" .env
+sed -i "s^{{\\\$POLR_ACCT_ACTIVATION}}^$POLR_ACCT_ACTIVATION^g" .env
+sed -i "s^{{\\\$POLR_GENERATED_AT}}^$POLR_GENERATED_AT^g" .env
+sed -i "s^{{\\\$POLR_SETUP_RAN}}^$POLR_SETUP_RAN^g" .env
+
+sed -i "s^{{\\\$ST_PUBLIC_INTERFACE}}^$ST_PUBLIC_INTERFACE^g" .env
+sed -i "s^{{\\\$ST_SHORTEN_PERMISSION}}^$ST_SHORTEN_PERMISSION^g" .env
+sed -i "s^{{\\\$ST_INDEX_REDIRECT}}^$ST_INDEX_REDIRECT^g" .env
+sed -i "s^{{\\\$ST_PASSWORD_RECOV}}^$ST_PASSWORD_RECOV^g" .env
+sed -i "s^{{\\\$ST_AUTO_API}}^$ST_AUTO_API^g" .env
+sed -i "s^{{\\\$ST_ANON_API}}^$ST_ANON_API^g" .env
+sed -i "s^{{\\\$ST_BASE}}^$ST_BASE^g" .env
+
+sed -i "s^{{\\\$MAIL_HOST}}^$MAIL_HOST^g" .env
+sed -i "s^{{\\\$MAIL_PORT}}^$MAIL_PORT^g" .env
+sed -i "s^{{\\\$MAIL_USERNAME}}^$MAIL_USERNAME^g" .env
+sed -i "s^{{\\\$MAIL_PASSWORD}}^$MAIL_PASSWORD^g" .env
+sed -i "s^{{\\\$MAIL_FROM_ADDRESS}}^$MAIL_FROM_ADDRESS^g" .env
+sed -i "s^{{\\\$MAIL_FROM_NAME}}^$MAIL_FROM_NAME^g" .env
+
+sed -i "s^{{\\\$TMP_SETUP_AUTH_KEY}}^$TMP_SETUP_AUTH_KEY^g" .env
+
 echo "* Starting nginx"
 service php5-fpm start && chmod a+rwx /var/run/php5-fpm.sock && nginx -g "daemon off;"
